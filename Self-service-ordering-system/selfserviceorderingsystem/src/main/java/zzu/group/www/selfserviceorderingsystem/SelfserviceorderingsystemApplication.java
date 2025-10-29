df526ce7a5a605448d535ff344f5a11889a165c3
coco-zzu <535180644@qq.com>
2025-10-29 19:07:32 +0800

    增加了架构

---
+++ Self-service-ordering-system/selfserviceorderingsystem/src/main/java/zzu/group/www/selfserviceorderingsystem/SelfserviceorderingsystemApplication.java
@@ -0,0 +1,13 @@
+package zzu.group.www.selfserviceorderingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfserviceorderingsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfserviceorderingsystemApplication.class, args);
+    }
+
+}
