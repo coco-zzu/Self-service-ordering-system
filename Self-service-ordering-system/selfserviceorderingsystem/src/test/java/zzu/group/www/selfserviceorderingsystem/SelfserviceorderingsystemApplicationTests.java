fb7be542aedbf08c01d521a101e5294faa71f46f
coco-zzu <535180644@qq.com>
2025-10-29 19:07:32 +0800

    Add files via upload

---
+++ Self-service-ordering-system/selfserviceorderingsystem/src/test/java/zzu/group/www/selfserviceorderingsystem/SelfserviceorderingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package zzu.group.www.selfserviceorderingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfserviceorderingsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
